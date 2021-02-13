8fb9507eb4126d48bf8a1f22ce62cec6aea580cc
Guillermo Montero <guilleandmont@gmail.com>
2021-02-13 12:50:03 -0400

    added comments

--- Pig-latin.js
+++ Pig-latin.js
@@ -1,11 +1,18 @@
 'use strict';
 
+//Input: a string with a word
+//Output: the string in Pig Latin (if the word starts with a vowel, add "way" to the end. If it starts with
+    //consonants move them to the end of the word and add "ay" to the end.)
 
+
+//This function turns that start with consonants to Pig Latin
 function consonants (str) {
     let regex = str.match(/^[^aeiou]+/);
     return str.replace(regex, "") + regex + "ay";
 }
 
+//This function checks if the word starts with a vowel. If it does, it adds "way". If it doesn't, 
+    //it applies the function "consonants" to the input.
 function translatePigLatin(str) {
     return (/^[aeiou]/.test(str)) ? str + "way" : consonants(str);
 }
