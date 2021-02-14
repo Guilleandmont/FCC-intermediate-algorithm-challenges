664a745cf9c8657b52af1b7a6cd76b39ac5915c7
Guillermo Montero <guilleandmont@gmail.com>
2021-02-14 16:12:35 -0400

    finished challenge

--- DNA-pairing.js
+++ DNA-pairing.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//Input: a string with letters representing the nitrogenous bases in DNA.
+//Output: an array with each base pair inside another array.
+
+//This function takes a letter (a base) and returns the corresponding nitrogenous base.
+function pairBases(base) {
+    switch(base){
+        case "C":
+            return "G";
+        case "G":
+            return "C";
+        case "A":
+            return "T";
+        case "T":
+            return "A";
+    }
+}
+
+//Use split to separate into individual letters, use map to put each input letter inside another array with
+    //its corresponding base.
+function pairElement(str) {
+    return str.split("").map(item => [item, pairBases(item)]);
+}
+  
+console.log(pairElement("TTGAG"));
+//output: [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]
