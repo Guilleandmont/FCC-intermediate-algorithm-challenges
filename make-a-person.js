87050f23a8552df6d4bec00b863fdb3c1a470744
Guillermo Montero <guilleandmont@gmail.com>
2021-02-22 10:52:00 -0400

    added comments

--- make-a-person.js
+++ make-a-person.js
@@ -1,26 +1,30 @@
 'use strict';
 
+//This challenge is different in that it doesn't ask you to create a function. Instead, this challenge
+    //asks you to create an object with certain methods. This is my solution using ES6 class syntax.
+
+//We suppose here that firstAndLast is a string of two words.
 class Person {
     constructor(firstAndLast) {
-        this.getFullName = function () {
+        this.getFullName = function () { //The full name becomes what you used to create the object.
             return firstAndLast;
         };
 
         this.getFirstName = function () {
-            return this.getFullName().split(" ")[0];
+            return this.getFullName().split(" ")[0]; //Splits full name and takes first element as first name
         };
         this.getLastName = function () {
-            return this.getFullName().split(" ")[1];
+            return this.getFullName().split(" ")[1]; //Same as above, but takes the last name
         };
         this.setFirstName = function (first) {
-            const replace = this.getFullName().replace((/\w+/), first);
-            this.getFullName = function () {
+            const replace = this.getFullName().replace((/\w+/), first); 
+            this.getFullName = function () { //Takes full name and replaces the first word.
                 return replace;
             }
         };
         this.setLastName = function (last) {
             const replace = this.getFullName().replace((/\w+$/), last);
-            this.getFullName = function () {
+            this.getFullName = function () { //Takes full name and replaces the last word.
                 return replace;
             }
         }
@@ -37,3 +41,4 @@ class Person {
 const bob = new Person('Bob Ross');
 bob.setFullName("Haskell Curry");
 console.log(bob.getFirstName());
+//outputs 'Haskell'
